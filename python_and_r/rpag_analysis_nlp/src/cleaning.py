a45db1c8249b4d95128759062c5d8d47ae706ff5
cmstorrsfox <cmstorrsfox@gmail.com>
2021-10-12 17:13:22 +0100

    working tokenised and pos tagged sentences in df

--- python_and_r/rpag_analysis_nlp/src/cleaning.py
+++ python_and_r/rpag_analysis_nlp/src/cleaning.py
@@ -18,6 +18,22 @@ comments_18_21 = pd.concat(comments_18_21)
 #remove na values
 comments_18_21 = comments_18_21.dropna(how="any", axis=0)
 
+#remove names, numbers and other special character (bit blunt - needs work)
+comments_18_21["comment"] = comments_18_21["comment"].str.replace("([A-Z]\w+('s)?)(\s[A-Z]\w+('s)?)?|[^\w\s]+|[\d]", "")
+
+#stopwords
+stopwords = stopwords.words("english")
 
 
+#apply tokenization to each comment
+def tokenize_and_pos_tag(comment):
+  sent_tokens = sent_tokenize(comment.strip())
+  word_tokens = [word_tokenize(word) for word in sent_tokens]
+  pos_tagged = [pos_tag(token) for token in word_tokens]
+  flattened = [val for sublist in pos_tagged for val in sublist]
+  stopwords_gone = [(word, pos) for (word, pos) in flattened if word not in stopwords]
+  return stopwords_gone
 
+comments_18_21["tokens"] = comments_18_21["comment"].apply(tokenize_and_pos_tag)
+
+print(comments_18_21["tokens"])
